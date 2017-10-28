9de2dd8ed8c9b18ebae9ca1290db1d5fdcb630ba
fengkan <fengkan@gmail.com>
2017-10-28 17:27:14 +0800

    init

---
+++ src/casio.js
@@ -0,0 +1,28 @@
+  document.querySelector('#btn_get_prize').onclick = function () {
+$('#overlay').show();
+  };
+  document.querySelector('#btn_close').onclick = function () {
+$('#overlay').hide();
+  };
+  document.querySelector('#btn_send').onclick = function () {
+name = $('#name').attr("value");
+phone = $('#phone').attr("value");
+
+            $.ajax({
+                url: 'http://101.132.146.155/users/gttddki',
+                data: {
+                    r: new Date().getTime()
+                },
+                dataType: 'json',
+                error: function(a,b){
+                    console.log(a,b, 'error');
+                },
+                success: function(a){
+                    console.log(a, 'success');
+                },
+                complete: function(a){
+                    console.log(a, 'complete');
+                },
+                timeout: 3000
+            });   
+  };
