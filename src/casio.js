a551103fafcbebe259811b73c7eccceef571ab6e
fengkan <fengkan@gmail.com>
2017-10-30 16:53:57 +0800

    user id and wx

--- src/casio.js
+++ src/casio.js
@@ -1,24 +1,3 @@
-  document.querySelector('#btn_send').onclick = function () {
-name = $('#name').attr("value");
-phone = $('#phone').attr("value");
-url = 'http://casio.maxantad.com/users/gttddki?name='+name+'&phone='+phone;
-            $.ajax({
-            type:"get",
-                url: url,
-                error: function(a,b){
-                  var  toast;
-                  toast = $('#toast-info').ZPopup('toast');
-                  toast.showToast('请稍后重试');
-                },
-                success: function(a){
-                  $('#input_form').hide();
-                  $('#byebyemsg').show();
-                },
-                timeout: 3000
-            });
-            return false;
-  };
-
 function getPrizeURL() {
     var prize;
     $.getJSON('http://casio.maxantad.com/users/gttddki/prize', function(data){
@@ -33,3 +12,33 @@ function getPrizeURL() {
     return url;
 }
 
+function fetchCoupon() {
+  window.location = "http://coupon.m.jd.com/coupons/show.action?key=c9400b2e688843119613987a5d4ed962&roleId=8697713&to=http://sale.jd.com/m/act/BoSnJdIValMH.html"
+}
+
+function generateUUID(){
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+    });
+    return uuid;
+};
+
+function setUserId() {
+      var LS = window.localStorage;
+      if (LS.getItem('casio_user_id') === null) {
+        uuid = generateUUID();
+        LS.setItem('casio_user_id', uuid);
+      } else {
+        uuid = LS.getItem('casio_user_id');
+      }
+      return uuid;
+}
+
+function getUserId() {
+      var LS = window.localStorage;
+      uuid = LS.getItem('casio_user_id');
+      return uuid;
+}
---
+++ src/wx.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+
+var userAgent = window.navigator.userAgent.toLowerCase();
+var isMicroMessenger = userAgent.indexOf("micromessenger") != -1;
+var self_url = window.location.href.split('#')[0];
+var self_encoded_url = encodeURIComponent(self_url);
+
+if(isMicroMessenger) {
+	$.ajax({
+		url: 'http://casio.maxantad.com/jweixin_signature?url='+self_encoded_url,
+		dataType: 'JSON'
+	}).done(function(data){
+		console.log(data);
+		wx.config({
+			debug: false, 
+			appId: "wxb73779df66a7661d", 
+			timestamp: data.timestamp, 
+			nonceStr: data.nonceStr, 
+			signature: data.signature,
+			jsApiList: ['onMenuShareTimeline','onMenuShareAppMessage'] 
+		});
+	}).fail(function(err){
+			console.log(err);
+	});
+
+	wx.ready(function(){
+		wx.onMenuShareTimeline({
+			title: "weixin timeline share title",
+			link: "http://casio.maxantad.com/index.html", 
+			imgUrl: "http://cdn.dev.egret.com/examples/wonderful_1_3/logo.png", 
+			success: function () {
+				console.log("onMenuShareTimeline success");
+			},
+			cancel: function () {
+			}
+		});
+
+		wx.onMenuShareAppMessage({
+			title: "weixin app share title", 
+			desc: "description content for share", 
+			link: "http://casio.maxantad.com/index.html",
+			imgUrl: "http://cdn.dev.egret.com/examples/wonderful_1_3/logo.png",
+			type: "", 
+			dataUrl: "", 
+			success: function () {
+				console.log("onMenuShareAppMessage success");
+			},
+			cancel: function () {
+			}
+		});
+
+	});
+
+} else {
+}
+
+    });
+
